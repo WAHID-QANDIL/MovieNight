02fa78c7e7f6877d151d649d1c5fd9b584fdc9b1
Wahid Ali <1213089@dtu.edu.eg>
2025-05-03 07:12:11 +0300

    Add scopes local composition

---
+++ app/src/main/java/org/wahid/movienight/presentation/core/LocalAnimatedVisibilityScope.kt
@@ -0,0 +1,6 @@
+package org.wahid.movienight.presentation.core
+
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.runtime.staticCompositionLocalOf
+
+val LocalAnimatedVisibilityScope = staticCompositionLocalOf<AnimatedVisibilityScope?> { null }
