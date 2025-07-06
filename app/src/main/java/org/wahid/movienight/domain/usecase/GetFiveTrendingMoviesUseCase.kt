f0ae2039548b85027338fb628cb2ac10687a8358
Wahid Ali <1213089@dtu.edu.eg>
2025-07-06 21:12:55 +0300

    Change return type of GetFiveTrendingMoviesUseCase from Flow to List

--- app/src/main/java/org/wahid/movienight/domain/usecase/GetFiveTrendingMoviesUseCase.kt
+++ app/src/main/java/org/wahid/movienight/domain/usecase/GetFiveTrendingMoviesUseCase.kt
@@ -1,6 +1,5 @@
 package org.wahid.movienight.domain.usecase
 
-import kotlinx.coroutines.flow.Flow
 import org.wahid.movienight.domain.model.Movie
 import org.wahid.movienight.domain.repository.MovieRepository
 import javax.inject.Inject
@@ -8,7 +7,7 @@ import javax.inject.Inject
 class GetFiveTrendingMoviesUseCase @Inject constructor(
     val repository: MovieRepository,
 ) {
-    suspend operator fun invoke(): Flow<Movie> {
+    suspend operator fun invoke(): List<Movie> {
         return repository.get5THTrendingMovies()
     }
 }
